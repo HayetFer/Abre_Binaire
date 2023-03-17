29ee64058fc99dd84f54b23f112fbf3edbdd3f91
Hayet <117082010+HayetFer@users.noreply.github.com>
2023-03-17 15:59:14 +0100

    Update ArbreBinaire.java

--- ArbreBinaire.java
+++ ArbreBinaire.java
@@ -1,10 +1,12 @@
 public class ArbreBinaire
 {
     public Noeud racine;
-    public ArbreBinaire(Noeud r){
+    public ArbreBinaire(Element e){
+        Noeud r = new Noeud(e);
         racine = r;
     }
-    public void ajout(Noeud a){
+    public void ajout(Element e){
+        Noeud a = new Noeud(e);
         ajout(a,racine);
     }
     public void ajout(Noeud a , Noeud courrant) {
@@ -65,21 +67,120 @@ public class ArbreBinaire
             }
         }
     }
+    public Noeud rechercheNoeud(int cle){
+        return rechercheNoeud(cle, racine);
+    }
+    public Noeud rechercheNoeud(int cle, Noeud courrant){
+        if (courrant.getCle() == cle) {
+            return courrant;
+        } else if (courrant.getCle() > cle) {
+            if (courrant.GetGauche() != null) {
+                return rechercheNoeud(cle, courrant.GetGauche());
+            } else {
+                return null;
+            }
+        } else {
+            if (courrant.GetDroite() != null) {
+                return rechercheNoeud(cle, courrant.GetDroite());
+            } else {
+                return null;
+            }
+        }
+    }
+
+    public void suppression(int cle){
+        //A CHANGER
+        if(rechercheNoeud(cle).isFeuille() && rechercheNoeud(cle)!=racine){
+            if(rechercheParent(cle).getCle()<cle){
+                rechercheParent(cle).setDroite(null);
+            }
+            else{
+                rechercheParent(cle).setGauche(null);
+            }
+        } else if (rechercheNoeud(cle).GetGauche()==null) {
+            if(rechercheParent(cle).getCle()<cle){
+                rechercheParent(cle).setDroite(rechercheNoeud(cle).GetDroite());
+            }
+            else{
+                rechercheParent(cle).setGauche(rechercheNoeud(cle).GetDroite());
+            }
+        } else if (rechercheNoeud(cle).GetDroite()==null) {
+            if(rechercheParent(cle).getCle()>cle){
+                rechercheParent(cle).setGauche(rechercheNoeud(cle).GetGauche());
+            }
+            else{
+                rechercheParent(cle).setDroite(rechercheNoeud(cle).GetGauche());
+            }
+        }
+        else{
+            Noeud parent = rechercheParent(cle);
+            if(parent.getCle()<cle){
+                parent.setDroite(rechercheMin(parent.GetDroite()));
+            }
+            else{
+                parent.setGauche(rechercheMin(parent.GetDroite()));
+            }
+        }
+    }
+    private void suppression(int cle, Noeud courrant){
+
+    }
+    private Noeud rechercheParent(int cle){
+        return rechercheParent(cle,racine);
+    }
+    private Noeud rechercheParent(int cle,Noeud courrant){
+
+        if (courrant.GetGauche()!=null && courrant.GetGauche().getCle()== cle) {
+            return courrant;
+        }
+        else if (courrant.GetDroite()!=null && courrant.GetDroite().getCle()== cle) {
+            return courrant; }
+
+        else if (courrant.getCle() > cle) {
+            if (courrant.GetGauche() != null) {
+                return rechercheParent(cle, courrant.GetGauche());
+            } else {
+                return null;
+            }
+        } else {
+            if (courrant.GetDroite() != null) {
+                return rechercheParent(cle, courrant.GetDroite());
+            } else {
+                return null;
+            }
+        }
+    }
+
+    private Noeud rechercheMin(Noeud n){
+        Noeud min = n;
+        while(min.GetGauche()!=null){
+            min=min.GetGauche();
+        }
+        return min;
+    }
     public static void main(String[] args) {
         Element e = new Element(10,15);
         Noeud n = new Noeud(e);
         Element e1 = new Element(20,18);
         Noeud n1 = new Noeud(e1);
-        Element e2 = new Element(20,16);
+        Element e2 = new Element(20,13);
         Noeud n2 = new Noeud(e2);
-        Element e3 = new Element(20,1);
+        Element e3 = new Element(20,2);
+        Element e4 = new Element(20,17);
+        Element e5 = new Element(20,20);
+        Element e6 = new Element(20,12);
         Noeud n3 = new Noeud(e3);
-        ArbreBinaire test = new ArbreBinaire(n);
-        test.ajout(n1);
-        test.ajout(n2);
-        test.ajout(n3);
+        ArbreBinaire test = new ArbreBinaire(e);
+        test.ajout(e1);
+        test.ajout(e2);
+        test.ajout(e3);
+        test.ajout(e4);
+        test.ajout(e5);
+        test.ajout(e6);
+        test.suppression(13);
         System.out.println(test.affichage());
-        System.out.println(test.recherche(19));
+        //System.out.println(test.rechercheMin(n));
+        //System.out.println(test.recherche(19));
 
     }
 
